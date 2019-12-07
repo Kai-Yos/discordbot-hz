0885c48e15ef2002a29ddc3795d1771a25138c9b
Kai-Yos <gamerxremagmc@gmail.com>
2019-12-07 23:58:25 +0100

    Ajout d'options pour le client + optimisation

---
+++ main.js
@@ -0,0 +1,16 @@
+const { Client } = require("discord.js");
+const client = new Client({ disableEveryone: true })
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") msg.channel.send("Pong!");
+  if (msg.content === "everyone")
+    msg.channel.send("@everyone, salut à tous !", {disableEveryone: false });
+  if (msg.content === "noteveryone") 
+    msg.channel.send("@everyone (noteveryone), salut à tous !");
+});
+
+client.login("NjUyOTIwMTM3NDA5NzU3MTk1.XewWLw.D31qe3MoVMkuDuxHqD3nGdgrsoU");
